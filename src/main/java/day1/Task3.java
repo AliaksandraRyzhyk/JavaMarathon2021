8be2d27d869f5c3c7746c34394e283306325c6b2
AliaksandraRyzhyk <ryzhikai@rambler.ru>
2021-01-20 11:04:18 +0300

    Add Task1-6 day1

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int value = 0;
+        while (value < 10){
+            System.out.println("JAVA"+" ");
+            value++;
+        }
     }
 }
