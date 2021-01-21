c9d44a8e6a3c49bcbfe02b7a64caadaa18a18562
AliaksandraRyzhyk <ryzhikai@rambler.ru>
2021-01-21 12:56:17 +0300

    Add Task1-4 day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество этажей в доме");
+        int x = sc.nextInt();
 
+        if (x <= 0){
+            System.out.println("Ошибка ввода");
+        } else  if(x < 5){
+            System.out.println("Малоэтажный дом");
+        } else  if (x > 8){
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Среднеэтажный дом");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите два числа, используя после каждого Enter");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        } else {
+            for (int i = a + 1; i < b; i++) {
+                while (i % 5 == 0 && i % 10 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
 }
+
+
