ce35f5e8013acf6fb45f2f219c3b2b5bcac412a3
AliaksandraRyzhyk <ryzhikai@rambler.ru>
2021-01-25 13:42:00 +0300

    Add task1-2 day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,41 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+    Car car = new Car();
+    car.setYear(1958);
+    car.setColor("red");
+    car.setModel("Ferrari");
+    System.out.println("Year: " + car.getYear());
+    System.out.println("Color: " + car.getColor());
+    System.out.println("Model: " + car.getModel());
     }
+static class Car {
+        private int year;
+        private String color;
+        private String model;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,33 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2018, "green","Honda");
+        System.out.println("Year: "+motorbike.getYear());
+        System.out.println("Color: "+motorbike.getColor());
+        System.out.println("Model: "+motorbike.getModel());
 
+    }
+    static class Motorbike{
+        private final int year;
+        private final String color;
+        private final String model;
+
+        public Motorbike(int year, String color, String model){
+            this.year = year;
+            this.color = color;
+            this.model = model;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public String getModel() {
+            return model;
+        }
     }
 }
