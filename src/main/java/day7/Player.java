784df938bc3daa1c39cfc997a2fb69c0f0171089
AliaksandraRyzhyk <ryzhikai@rambler.ru>
2021-01-26 20:00:06 +0300

    Add task1-2 day7

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,50 @@
+package day7;
+
+public class Player {
+    private int stamina;
+    public static final int MAX_STAMINA = 100;
+    public static final int MIN_STAMINA = 0;
+    public static int countPlayers = 0;
+
+    public Player(int stamina){
+        if (countPlayers < 6) {
+        this.stamina = stamina;
+        countPlayers++;
+        } else countPlayers = 6;
+    }
+
+    public void run() {
+        if (stamina > MIN_STAMINA) {
+            this.stamina = stamina - 1;
+            if (stamina == MIN_STAMINA) countPlayers--;
+        }
+    }
+
+    public static void info(){
+        int i = 6-countPlayers;
+        if (countPlayers < 6){
+            switch (i){
+                case 0:
+                case 5:
+                    System.out.println("Команды неполные. На поле еще есть "+i+" свободных мест");
+                    break;
+                case 1:
+                    System.out.println("Команды неполные. На поле еще есть "+i+" свободное место");
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println("Команды неполные. На поле еще есть "+i+" свободных места");
+                    break;
+            }
+        } else System.out.println("На поле нет свободных мест");
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+}
--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane("Boing",2020,70,420);
+        Airplane airplane1 = new Airplane("Airbus", 2006,50,350);
+        Airplane.compareAirplanes(airplane,airplane1);
     }
 }
